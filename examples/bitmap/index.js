220cf5d581970f899b5dd0a67aa65bc204c10481
Guilherme Avila <guilhermebav@gmail.com>
2024-04-25 21:13:38 -0300

    Examples: validate that ImageBitmap interface doesn't work for .png files

--- examples/bitmap/index.js
+++ examples/bitmap/index.js
@@ -5,10 +5,10 @@ import { CanvasTexture } from "../../src/renderer/objects/canvas_texture.mjs";
 import { Mesh } from "../../src/renderer/objects/mesh.mjs";
 import { Shader } from "../../src/renderer/objects/shader.mjs";
 import { Material } from "../../src/renderer/objects/material.mjs";
-
-import texture_shader from "../shaders/texture_shader.mjs";
-import { Texture } from "../../src/renderer/objects/texture.mjs";
 import { Sampler } from "../../src/renderer/objects/sampler.mjs";
+import { Texture } from "../../src/renderer/objects/texture.mjs";
+
+import bitmap_shader from "../shaders/bitmap_shader.mjs";
 
 let renderer, canvas, scene;
 let render_pass, render_target;
@@ -16,7 +16,7 @@ let render_pass, render_target;
 const dpr = window.devicePixelRatio;
 let viewport = {x: window.innerWidth * dpr | 0, y: window.innerHeight * dpr | 0};
 
-const init = async () => {
+const init = async (bitmap) => {
   renderer = new Renderer(await Renderer.acquire_device());
 
   const canvas_texture = new CanvasTexture();
@@ -24,41 +24,75 @@ const init = async () => {
   canvas = canvas_texture.canvas;
   document.body.append(canvas);
 
-  const tex = new Texture({
-    size: { width: 1, height: 1 },
+  const tex1 = new Texture({
+    size: { width: 2, height: 1 },
     format: "rgba8unorm",
   });
+
+  const data_tex = renderer.cache.get_texture(tex1);
+  const data_gpu = renderer.backend.resources.get_texture(data_tex.bid).texture;
+  renderer.backend.device.queue.writeTexture(
+    { texture: data_gpu },
+    new Uint8Array([
+      255, 0, 0, 255,
+      0, 128 * 64./255, 0, 64
+    ]),
+    {},
+    tex1.size,
+  );
+
+  const tex2 = new Texture({
+    size: { width: 2, height: 1 },
+    format: "rgba8unorm-srgb",
+  });
+
+  const srgb_tex = renderer.cache.get_texture(tex2);
+  const srgb_gpu = renderer.backend.resources.get_texture(srgb_tex.bid).texture;
+  renderer.backend.device.queue.copyExternalImageToTexture(
+    { source: bitmap },
+    { texture: srgb_gpu, premultipliedAlpha: true },
+    { width: bitmap.width, height: bitmap.height },
+  );
+
+  const tex3 = new Texture({
+    size: { width: 2, height: 1 },
+    format: "rgba8unorm-srgb",
+  });
+
+  const ext_tex = renderer.cache.get_texture(tex3);
+  const ext_gpu = renderer.backend.resources.get_texture(ext_tex.bid).texture;
+  renderer.backend.device.queue.copyExternalImageToTexture(
+    { source: bitmap },
+    { texture: ext_gpu, premultipliedAlpha: false },
+    { width: bitmap.width, height: bitmap.height },
+  );
 
   render_pass = new RenderPass({
     formats: {
       color: [navigator.gpu.getPreferredCanvasFormat()],
     },
     bindings: [
-      { binding: 0, name: "tex", resource: tex },
-      { binding: 1, name: "sampler", resource: new Sampler() },
+      { binding: 0, name: "sampler", resource: new Sampler( {
+        filtering: { mag: "linear", min: "nearest" },
+      } ) },
+      { binding: 1, name: "data_tex", resource: tex1 },
+      { binding: 2, name: "srgb_tex", resource: tex2 },
+      { binding: 3, name: "ext_tex", resource: tex3 },
     ]
   });
-
-  const tex_cache = renderer.cache.get_texture(tex);
-  const gpu_tex = renderer.backend.resources.get_texture(tex_cache.bid).texture;
-  renderer.backend.device.queue.writeTexture(
-    { texture: gpu_tex },
-    new Uint8Array([128, 128, 128, 255]),
-    {}, { width: 1, height: 1 }
-  );
 
   render_target = new RenderTarget({
     pass: render_pass,
     size: { width: viewport.x, height: viewport.y },
     color: [ 
-      { texture: canvas_texture, clear: [.05, .05, .05, 1] }
+      { texture: canvas_texture, clear: [.5, .5, .5, 1] }
     ],
   });
 
   render_pass.set_render_target(render_target);
 
   const material = new Material({
-    shader: new Shader({code: texture_shader}),
+    shader: new Shader({code: bitmap_shader}),
   });
 
   const full_quad = new Mesh({
@@ -72,8 +106,6 @@ const init = async () => {
 
   animate();
 }
-
-init();
 
 const auto_resize = () => {
   const dpr = window.devicePixelRatio;
@@ -92,3 +124,11 @@ const animate = () => {
   auto_resize();
   renderer.render(render_pass, scene);
 }
+
+(async function loadImageBitmap(url) {
+  const res = await fetch(url);
+  const blob = await res.blob();
+  init(await createImageBitmap(blob, { premultiplyAlpha: 'none' }));
+})('../textures/2px.png');
+
+// init();
---
+++ examples/shaders/bitmap_shader.mjs
@@ -0,0 +1,42 @@
+export default `
+struct FragInput {
+  @builtin(position) position : vec4f,
+  @location(0) uv : vec2f,
+}
+
+@vertex fn vs(@builtin(vertex_index) vertexIndex : u32) -> FragInput {
+  var output : FragInput;
+
+  let pos = array(
+    vec2f( 0,  4),  // top center
+    vec2f(-4, -4),  // bottom left
+    vec2f( 4, -4)   // bottom right
+  );
+
+  let c_pos = pos[vertexIndex];
+  output.position = vec4f(c_pos, .5, 1);
+  output.uv = (c_pos + 1) * .5;
+
+  return output;
+}
+
+@group(0) @binding(0) var usampler: sampler;
+@group(0) @binding(1) var data_tex: texture_2d<f32>;
+@group(0) @binding(2) var srgb_tex: texture_2d<f32>;
+@group(0) @binding(3) var ext_tex: texture_2d<f32>;
+
+@fragment fn fs(in : FragInput) -> @location(0) vec4f {
+  let x = clamp(in.uv.x * 2. - .5, 0, 1);
+  const color1 = vec3f(1, 0, 0);
+  const color2 = vec3f(0, .5, 0) * .25;
+  var color = mix(color1, color2, x);
+  let t_color = textureSample(data_tex, usampler, vec2f(in.uv.x, .5));
+  let e_color = textureSample(ext_tex, usampler, vec2f(in.uv.x, .5));
+  let s_color = textureSample(srgb_tex, usampler, vec2f(in.uv.x, .5));
+
+  if (in.uv.y > .75) { color = s_color.rgb; }
+  else if (in.uv.y > .5) { color = e_color.rgb * e_color.a; }
+  else if (in.uv.y > .25) { color = t_color.rgb; }
+
+  return vec4f(pow(color, vec3f(1./2.2)), 1.);
+}`;
