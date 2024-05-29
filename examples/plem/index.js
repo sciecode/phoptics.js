455a1348a7f8456cd8b94bace724c0a4726b8174
Guilherme Avila <guilhermebav@gmail.com>
2024-05-29 20:12:16 -0300

    Examples: plem basic

--- examples/plem/index.js
+++ examples/plem/index.js
@@ -3,6 +3,7 @@ import { Engine, Mesh, RenderList, Shader, Sampler, Material, Texture, Geometry,
 import { Vec4 } from 'phoptics/math';
 
 import mipmap_shader from "../shaders/mipmap_shader.mjs";
+import filtering_shader from "../shader/filtering_shader.mjs";
 
 const dpr = window.devicePixelRatio;
 let viewport = { width: window.innerWidth * dpr | 0, height: window.innerHeight * dpr | 0 };
@@ -90,6 +91,59 @@ const generate_mipmap_cubemap = (engine, original) => {
   return cubemap;
 }
 
+const generate_plem = (engine, cubemap) => {
+  const tex_size = cubemap.size.width, mips = Texture.max_mip_levels(tex_size) - 4; // MAX - MIN
+  const plem = new Texture({
+    format: cubemap.format,
+    size: { width: tex_size, height: tex_size, depth: 6 },
+    mip_levels: mips,
+  });
+
+  const render_pass = new RenderPass({
+    formats: { color: [cubemap.format] },
+    bindings: [
+      { binding: 0, name: "sampler", resource: new Sampler({ filtering: { min: "linear", mag: "linear" } }) },
+      { binding: 1, name: "cube", resource: cubemap.create_view() },
+      { binding: 2, name: "globals", resource: globals }
+    ]
+  });
+
+  const render_target = new RenderTarget({
+    color: [{ view: undefined, clear: [.0, .0, .0, 0] }],
+  });
+
+  render_pass.set_render_target(render_target);
+
+  scene = new RenderList();
+  const quad = new Mesh(
+    new Geometry({ draw: { count: 3 } }),
+    new Material({ shader: new Shader({ code: filtering_shader }) })
+  );
+  scene.add(quad);
+
+  // mips
+  for (let m = 0; m < mips; m++) {
+    const roughness = m / (mips - 1);
+    render_pass.bindings.cube = cubemap.create_view({ dimension: "cube" });
+    render_pass.bindings.update();
+    for (let i = 0; i < 6; i++) {
+      globals.info.set(i, roughness); // face / lod
+      globals.update();
+      render_target.color[0].view = plem.create_view({
+        dimension: "2d",
+        arrayLayerCount: 1,
+        mipLevelCount: 1,
+        baseArrayLayer: i,
+        baseMipLevel: m,
+      })
+
+      engine.render(render_pass, scene);
+    }
+  }
+
+  return plem;
+}
+
 (async () => {
   engine = new Engine(await Engine.acquire_device());
 
@@ -104,6 +158,7 @@ const generate_mipmap_cubemap = (engine, original) => {
   for (let i = 0; i < bitmaps.length; i++) engine.upload_texture(original, bitmaps[i], { target_origin: [0, 0, i] });
 
   const cubemap = generate_mipmap_cubemap(engine, original);
+  const plem = generate_plem(engine, cubemap);
 
   original.destroy();
 
