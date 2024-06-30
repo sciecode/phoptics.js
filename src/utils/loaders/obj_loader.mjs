eacc3dafdfd13476407b986ed7477381900f471d
Guilherme Avila <guilhermebav@gmail.com>
2024-06-30 16:23:49 -0300

    OBJLoader: integrate with phoptics geometry

--- src/utils/loaders/obj_loader.mjs
+++ src/utils/loaders/obj_loader.mjs
@@ -1,3 +1,4 @@
+import { Geometry, Vertex } from 'phoptics';
 
 const parse_faces = (info) => {
   do {
@@ -60,24 +61,50 @@ export class OBJLoader {
         }
       }
 
-      const stride = 3 + 3 + 2;
       const vert_count = info.indices.length;
-      const data = new Float32Array(vert_count * stride);
+      let vertices = [];
 
-      for (let i = 0; i < vert_count; i++) {
-        let [ipos, iuv, inorm] = info.indices[i];
-        const i8 = i * 8;
-        data[i8] = info.positions[ipos][0];
-        data[i8 + 1] = info.positions[ipos][1];
-        data[i8 + 2] = info.positions[ipos][2];
-        data[i8 + 3] = info.normals[inorm][0];
-        data[i8 + 4] = info.normals[inorm][1];
-        data[i8 + 5] = info.normals[inorm][2];
-        data[i8 + 6] = info.uvs[iuv][0] % 1;
-        data[i8 + 7] = info.uvs[iuv][1] % 1;
+      if (info.positions.length) {
+        const pos = { data: new Float32Array(3 * vert_count), stride: 12 };
+        for (let i = 0; i < vert_count; i++) {
+          let [ipos, iuv, inorm] = info.indices[i];
+          const i3 = i * 3;
+          pos.data[i3] = info.positions[ipos][0];
+          pos.data[i3 + 1] = info.positions[ipos][1];
+          pos.data[i3 + 2] = info.positions[ipos][2];
+        }
+        vertices.push(new Vertex(pos));
+      }
+      
+      if (info.normals.length) {
+        const norm = { data: new Float32Array(3 * vert_count), stride: 12 };
+        for (let i = 0; i < vert_count; i++) {
+          let [ipos, iuv, inorm] = info.indices[i];
+          const i3 = i * 3;
+          norm.data[i3] = info.normals[inorm][0];
+          norm.data[i3 + 1] = info.normals[inorm][1];
+          norm.data[i3 + 2] = info.normals[inorm][2];
+        }
+        vertices.push(new Vertex(norm));
       }
 
-      return { data, count: vert_count, stride: 32 };
+      if (info.uvs.length) {
+        const uv = { data: new Float32Array(2 * vert_count), stride: 8 };
+        for (let i = 0; i < vert_count; i++) {
+          let [ipos, iuv, inorm] = info.indices[i];
+          const i3 = i * 3;
+          uv.data[i3] = info.uvs[iuv][0] % 1;
+          uv.data[i3 + 1] = info.uvs[iuv][1] % 1;
+        }
+        vertices.push(new Vertex(uv));
+      }
+
+      const geo = new Geometry({
+        draw: { count: vert_count },
+        vertices
+      });
+
+      return geo;
 
     });
   }
