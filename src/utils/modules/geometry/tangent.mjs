77b55ee4c9194aa5b0077e54f44eec772b1fadd1
Guilherme Avila <guilhermebav@gmail.com>
2024-07-02 23:51:41 -0300

    MikkTSpace: use phoptics math - partial

--- src/utils/modules/geometry/tangent.mjs
+++ src/utils/modules/geometry/tangent.mjs
@@ -1,4 +1,5 @@
 import { Vertex } from 'phoptics';
+import { Vec3, Vec2 } from 'phoptics/math';
 import { opt_remap } from 'phoptics/utils/modules/geometry/optimizer.mjs';
 import { TYPE } from "./common/type.mjs";
 import { Memory, memcpy } from './common/memory.mjs';
@@ -43,84 +44,78 @@ class Info {
     this.neighbours = [-1, -1, -1];
   	this.groups = [-1, -1, -1];
 
-    this.s = { x: 0, y: 0, z: 0, m: 0 }; // TODO: use phoptics-math
-    this.t = { x: 0, y: 0, z: 0, m: 0 };
+    this.s = new Vec3(); this.sm = 0;
+    this.t = new Vec3(); this.tm = 0;
+    
   	this.any = true;
     this.preserve = false;
   }
 }
 
-const add = (a, b) => { return { x: a.x+b.x, y: a.y+b.y, z: a.z+b.z } }
-const sub = (a, b) => { return { x: a.x-b.x, y: a.y-b.y, z: a.z-b.z } }
-const scl = (s, a) => { return { x: s * a.x, y: s * a.y, z: s * a.z } }
-const len_sqr = (a) => a.x*a.x + a.y*a.y + a.z*a.z;
-const len = (a) => Math.sqrt(len_sqr(a));
+const non_zero = (v) => Math.abs(v) > Number.EPSILON;
 
 // assumes no degenerate faces
 export const generate_tangents = (geometry, info) => {
   opt_remap(geometry); // make indexed & remove duplicates
-  const triangle_count = (indices.length / 3 | 0);
-  const index_count = triangle_count * 3;
-
-  const get_uv = (idx) => {
+  
+  const get_uv = (idx, v) => {
     const buffer = geometry.attributes.vertices[info.uv.id];
-    const pos = idx * buffer.stride + (info.uv.offset || 0);
-    return { x: buffer[pos], y: buffer[pos + 1] };
+    v.from(buffer.data, idx * buffer.stride + (info.uv.offset || 0));
   }
   
-  const get_pos = (idx) => {
+  const get_pos = (idx, v) => {
     const buffer = geometry.attributes.vertices[info.position.id];
-    const pos = idx * buffer.stride + (info.position.offset || 0);
-    return { x: buffer[pos], y: buffer[pos + 1], z: buffer[pos + 2] };
+    v.from(buffer.data, idx * buffer.stride + (info.position.offset || 0));
   }
 
-  const get_normal = (idx) => {
+  const get_normal = (idx, v) => {
     const buffer = geometry.attributes.vertices[info.normal.id];
-    const pos = idx * buffer.stride + (info.normal.offset || 0);
-    return { x: buffer[pos], y: buffer[pos + 1], z: buffer[pos + 2] };
+    v.from(buffer.data, idx * buffer.stride + (info.normal.offset || 0));
   }
 
+  const indices = geometry.index.data;
+  const triangle_count = (indices.length / 3 | 0);
   const tri_info = new Array(triangle_count);
+
   for (let i = 0; i < triangle_count; i++)
     tri_info[i] = new Info();
 
-
   // initialize triangle list info
-  const indices = geometry.index.data;
-  for (let i = 0; i < triangle_count; i++) {
-    const i3 = i * 3;
-    const idx1 = indices[i3], idx2 = indices[i3 + 1], idx3 = indices[i3 + 2];
-    const v1 = get_pos(idx1), v2 = get_pos(idx2), v3 = get_pos(idx3);
-    const t1 = get_uv(idx1), t2 = get_uv(idx2), t3 = get_uv(idx3);
+  {
+    const vs = new Vec3(), vt = new Vec3();
+    const v1 = new Vec3(), v2 = new Vec3(), v3 = new Vec3();
+    const t1 = new Vec2(), t2 = new Vec2(), t3 = new Vec2();
+    for (let i = 0; i < triangle_count; i++) {
+      const i3 = i * 3, info = tri_info[i];
+      const idx1 = indices[i3], idx2 = indices[i3 + 1], idx3 = indices[i3 + 2];
 
-    const t21x = t2.x - t1.x, t21y = t2.y - t1.y;
-		const t31x = t3.x - t1.x, t31y = t3.y - t1.y;
-		const d1 = sub(v2, v1), d2 = sub(v3, v1);
+      get_uv(idx1, t1), get_uv(idx2, t2), get_uv(idx3, t3);
+      const t21 = t2.sub(t1), t31 = t3.sub(t1);
+      const area = t21.x * t31.y - t21.y * t31.x;
+      info.preserve = area > 0;
 
-    const abs_area = Math.abs(t21x * t31y - t21y * t31x);
-		const vs = sub(scl(t31y, d1), scl(t21y, d2));
-		const vt = add(scl(-t31x, d1), scl(t21x, d2));
+      if (non_zero(abs_area)) {
+        get_pos(idx1, v1), get_pos(idx2, v2), get_pos(idx3, v3);
+    		const d1 = v2.sub(v1), d2 = v3.sub(v1);
+        
+        const lens = vs.copy(d1).mul_f32(t31.y)
+          .sub(v1.copy(d2).mul_f32(t21.y))
+          .length();
+        
+        const lent = vt.copy(d1).mul_f32(-t31.x)
+          .add(v1.copy(d2).mul_f32(t21.x))
+          .length();
+        
+  			const sign = info.preserve ? -1 : 1;
+  			if (non_zero(lens)) info.s.copy(vs).mul_f32(sign / lens);
+  			if (non_zero(lent)) info.t.copy(vt).mul_f32(sign / lent);
 
-    const info = tri_info[i];
-    info.preserve = area > 0;
+        const abs_area = Math.abs(area);
+        info.sm = lens / abs_area;
+        info.tm = lent / abs_area;
 
-    if (abs_area > Number.EPSILON) {
-      const lens = len(vs), lent = len(vt);
-			const sign = info.preserve ? -1 : 1;
-			if (lens > Number.EPSILON) {
-        const ns = scl(sign / lens, vs);
-        info.s.x = ns.x; info.s.y = ns.y; info.s.z = ns.z;
+  			if (non_zero(info.sm) && non_zero(info.tm)) info.any = false;
       }
-      
-			if (lent > Number.EPSILON) {
-        const ts = scl(sign / lent, vt);
-        info.t.x = ts.x; info.t.y = ts.y; info.t.z = ts.z;
-      }
-      
-      info.s.m = lens / abs_area;
-      info.t.m = lent / abs_area;
-			if (info.s.m > Number.EPSILON && info.t.m > Number.EPSILON)
-				info.any = false;
     }
   }
 
