5158bce7c70506eeea25fcac2e7ec2a315454466
Guilherme Avila <guilhermebav@gmail.com>
2024-07-11 14:02:28 -0300

    ShaderLib: impl shader registry

--- src/utils/modules/shaders/shader_lib.mjs
+++ src/utils/modules/shaders/shader_lib.mjs
@@ -1,14 +1,22 @@
+import { Shader } from 'phoptics';
 
 export class ShaderLib {
   constructor(modules) {
     this.modules = {};
-    if (modules) this.load(modules);
+    this.shaders = new Map();
+    if (modules) this.load_modules(modules);
   }
-  load(modules) {
+  load_modules(modules) {
     for (let name in modules)
       this.modules[name] = modules[name];
   }
-  generate(shader) {
+  register(shaders) {
+    for (let name in shaders)
+      this.shaders.set(name, this.process(shaders[name]));
+  }
+  get(name) { return this.shaders.get(name); }
+  destroy(name) { this.shaders.get(name).destroy(); this.shaders.delete(name); }
+  process(shader) {
     let import_map = {}, pos = shader.indexOf('@import');
     while (pos > -1) {
       const st = pos;
@@ -17,26 +25,25 @@ export class ShaderLib {
       let comma = shader.indexOf(',', pos);
       while (comma + 1 && comma < delim) {
         const name = shader.substring(pos, comma).trim();
-        const module = this.modules[name];
-        if (!module) throw `ShaderLib: module '${name}' not loaded.`;
-        if (!import_map[name]) {
-          shader = shader.concat(module);
-          import_map[name] = true;
-        }
+        shader = this.#append(name, shader, import_map);
         pos = comma + 1;
         comma = shader.indexOf(',', pos);
       }
 
       if (!(delim + 1)) throw `ShaderLib: invalid @import syntax.`;
       const name = shader.substring(pos, delim).trim();
-      const module = this.modules[name];
-      if (!module) throw `ShaderLib: module '${name}' not loaded.`;
+      shader = this.#append(name, shader, import_map);
       shader = shader.substring(0, st).concat(shader.substring(delim + 1));
-      if (!import_map[name]) {
-        shader = shader.concat(module);
-        import_map[name] = true;
-      }
       pos = shader.indexOf('@import', delim + 1);
+    }
+    return new Shader({ code: shader });
+  }
+  #append(name, shader, import_map) {
+    const module = this.modules[name];
+    if (!module) throw `ShaderLib: module '${name}' not loaded.`;
+    if (!import_map[name]) {
+      shader = shader.concat(module);
+      import_map[name] = true;
     }
     return shader;
   }
